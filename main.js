82d7bfbe21c252ae0684fd6cb3a720500da64fb3
Brian Borin <Brianborin@icloud.com>
2017-08-24 15:54:20 -0700

    adding more projects to portfolio

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $('#wgDiv').hover(function(){
+    $('#watergate').fadeIn(1200).show();
+  });
+  $('#bbDiv').hover(function(){
+    $('#baseball').fadeIn(1200).show();
+  });
+  $('#bsDiv').hover(function(){
+    $('#bigSur').fadeIn(1200).show();
+  });
+  $('#agDiv').hover(function(){
+    $('#adam').fadeIn(1200).show();
+  });
+  $('#mcDiv').hover(function(){
+    $('#mccabes').fadeIn(1200).show();
+  });
+})
---
+++ musician/main.js
@@ -0,0 +1,12 @@
+let desc = document.querySelector('.desc');
+let songList = document.querySelector('.songList');
+let heroText = document.querySelector('.hero-text');
+
+function showTunes() {
+  desc.style.display = 'none';
+  songList.style.display = 'block';
+  // songList.style.marginLeft = "-200px";
+  heroText.style.marginRight = "200px";
+}
+
+console.log(desc);
